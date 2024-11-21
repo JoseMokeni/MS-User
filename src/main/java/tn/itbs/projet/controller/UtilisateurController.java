efb190f32832a6e55db9e944f79c9fb402921093
Jose Mokeni <jmmokeni@gmail.com>
2024-11-21 21:35:47 +0100

    bug fixes

--- src/main/java/tn/itbs/projet/controller/UtilisateurController.java
+++ src/main/java/tn/itbs/projet/controller/UtilisateurController.java
@@ -1,5 +1,6 @@
 package tn.itbs.projet.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/utilisateur")
+@AllArgsConstructor
 public class UtilisateurController {
 
     private UtilisateurService utilisateurService;
